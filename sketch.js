6cd5427dda7674e0cde2e06f126c071d37e462b4
Jonesh Shrestha <joneshshrestha69@gmail.com>
2018-05-28 13:30:34 +0545

    graphing perlin noise

--- sketch.js
+++ sketch.js
@@ -1,5 +1,7 @@
-let xoff1 = 0;
-let xoff2 = 10000;
+// let xoff1 = 0;
+// let xoff2 = 10000;
+let inc = 0.01
+let start = 0
 
 function setup() {
 	createCanvas(400, 400);
@@ -8,11 +10,23 @@ function setup() {
 function draw() {
 	background(51);
 
-	var x = map(noise(xoff1), 0, 1, 0, width);	
-	var y = map(noise(xoff2), 0, 1, 0, height);
-	xoff1 += 0.01;
-	xoff2 += 0.01;
+	// var x = map(noise(xoff1), 0, 1, 0, width);	
+	// var y = map(noise(xoff2), 0, 1, 0, height);
+	// xoff1 += 0.01;
+	// xoff2 += 0.01;
 
-	ellipse(x, y, 24, 24);
+	// ellipse(x, y, 24, 24);
+	stroke(255)
+	noFill()
+	beginShape()
+	let xoff = start
+	for (let x = 0; x < width; x++) {
+		stroke(255)
+		let y = noise(xoff) * height
+		vertex(x, y)
+		xoff += 0.01
+	}
+	endShape();
+	start += inc
 
 }
