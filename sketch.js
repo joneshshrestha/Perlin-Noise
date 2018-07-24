fd04c5a0d953fa2977610073a11c925c60320a84
Jonesh Shrestha <joneshshrestha69@gmail.com>
2018-07-24 17:27:11 +0545

    flow field

--- sketch.js
+++ sketch.js
@@ -1,39 +1,65 @@
-let inc = 0.1
-let scl = 10
-let cols, rows
+// Daniel Shiffman
+// http://codingtra.in
+// http://patreon.com/codingtrain
+// Code for: https://youtu.be/BjoM9oKOAKY
 
-let zoff = 0
+var inc = 0.1;
+var scl = 10;
+var cols, rows;
 
-let fr
+var zoff = 0;
+
+var fr;
+
+var particles = [];
+
+var flowfield;
 
 function setup() {
-	createCanvas(200, 200)
-	cols = floor(width / scl)
-	rows = floor(height / scl)
-	fr = createP('')
+  createCanvas(400, 400);
+  colorMode(HSB, 255);
+  cols = floor(width / scl);
+  rows = floor(height / scl);
+  fr = createP('');
+
+  flowfield = new Array(cols * rows);
+
+  for (var i = 0; i < 300; i++) {
+    particles[i] = new Particle();
+  }
+  background(51);
 }
 
 function draw() {
-	background(255)
-	let yoff = 0
-	for (let y = 0; y < rows; y++) {
-		let xoff = 0
-		for (let x = 0; x < cols; x++) {
-			let index = (x + y * width) * 4
-			let angle = noise(xoff, yoff, zoff) * TWO_PI
-			let v = p5.Vector.fromAngle(angle)
-			xoff += inc
-			stroke(0)
-			push()
-			translate(x * scl, y * scl)
-			rotate(v.heading())
-			line(0, 0, scl, 0)
+  var yoff = 0;
+  for (var y = 0; y < rows; y++) {
+    var xoff = 0;
+    for (var x = 0; x < cols; x++) {
+      var index = x + y * cols;
+      var angle = noise(xoff, yoff, zoff) * TWO_PI * 4;
+      var v = p5.Vector.fromAngle(angle);
+      v.setMag(1);
+      flowfield[index] = v;
+      xoff += inc;
+      stroke(0, 50);
+      // push();
+      // translate(x * scl, y * scl);
+      // rotate(v.heading());
+      // strokeWeight(1);
+      // line(0, 0, scl, 0);
+      // pop();
+    }
+    yoff += inc;
 
-			pop()
-		}
-		yoff += inc
-		zoff += 0.001
-	}
+    zoff += 0.0003;
+  }
 
-	fr.html(floor(frameRate()))
+  for (var i = 0; i < particles.length; i++) {
+    particles[i].follow(flowfield);
+    particles[i].update();
+    particles[i].edges();
+    particles[i].show();
+  }
+
+  fr.html(floor(frameRate()));
 }
